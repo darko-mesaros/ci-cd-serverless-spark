a7decf54dd04ff789562faccb375dc6f10f7b7d0
darko-mesaros <darko.subotica@gmail.com>
2023-03-10 07:40:11 -0800

    Please fail this test

--- pyspark/tests/test_basic.py
+++ pyspark/tests/test_basic.py
@@ -1,4 +1,5 @@
 from jobs.extreme_weather import ExtremeWeather
 def test_extract_latest_daily_value(mock_views_df):
     ew = ExtremeWeather()
-    assert ew.findLargest(mock_views_df, "TEMP").TEMP == 44.1
+    #assert ew.findLargest(mock_views_df, "TEMP").TEMP == 44.1
+    assert ew.findLargest(mock_views_df, "TEMP").TEMP == 55.1
